eee9e0ee2e07221e388e6b90ed40e5672a288df0
Dave Gerrard <dg509@cam.ac.uk>
2017-10-05 08:18:31 +0100

    Added stub tests and classes to start on Issue #3

--- tests_integration/test_altmetric_request.py
+++ tests_integration/test_altmetric_request.py
@@ -1,9 +1,21 @@
+from altmetric_client.altmetric_request import AltmetricRequest
+from altmetric_client.url_builder import URLBuilder
+
 
 class TestAlmetricRequest:
 
-    '''Test class for the object that will call the Altmetric API.
-    Stub added to remind me to put these tests in a completely separate directory so that it's easier to exclude them
+    '''Test class for the object that calls the Altmetric API.
+    These tests have been put in a completely separate directory so that it's easier to exclude them
     from other test runs. Otherwise, simply running all the tests is going to hit the Altmetric endpoint all the time.
+    The tests only check a couple of the aspects of the returned JSON that aren't going to change. There are other tests
+    using locally-stored JSON (which also won't change) in the Unit Tests in the main tests folder, which actually test
+    the full JSON-parsing code base.
+    
+    All the configuration is also loaded from the project config in this integration test, too, so that we don't end up
+    putting our API key in the codebase. :) ... so running this test is also a good way to check that you've got your
+    config setup correctly.
     '''
 
-    pass
+    def setup_method(self):
+
+        self._test_altmetric_request
