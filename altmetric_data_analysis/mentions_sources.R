fa01bf575d0f7b66dbd13195daf4de40e9a1e524
Dave Gerrard <dg509@cam.ac.uk>
2017-11-23 08:09:31 +0000

    Adds basic filter for more mention analyis - Issue #14

--- altmetric_data_analysis/mentions_sources.R
+++ altmetric_data_analysis/mentions_sources.R
@@ -6,6 +6,18 @@ library(tidyverse)
 
 mentions <- read_csv("../files_out/20171122_0807_mentions.csv")
 
+# Basic plot of the sources in a bar chart
+
 ggplot(data = mentions) +
   geom_bar(mapping = aes(x = source)) +
   coord_flip()
+
+# Puts a view on mentions filtered by source
+
+filter(mentions, source == 'linkedin') %>%
+  View()
+
+# TODO Plot a histogram / freq_poly of tweet freqencies
+# TODO Facets of mention frequency per article for different channels... (this will be hard as Twitter stomps on everything)
+# TODO Facets of tweet frequency for a subset of similar papers (maybe)?
+
