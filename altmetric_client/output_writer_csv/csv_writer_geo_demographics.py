3c2b49bdaa61e39248f80bcfcffa2f07cf170af8
Dave Gerrard <dg509@cam.ac.uk>
2018-02-05 06:57:27 +0000

    Adds geo_demographics CSV writing for Issue #27

--- altmetric_client/output_writer_csv/csv_writer_geo_demographics.py
+++ altmetric_client/output_writer_csv/csv_writer_geo_demographics.py
@@ -1,33 +1,31 @@
 from csv import DictWriter
 from altmetric_client.output_writer_csv.csv_writer_base import CSVWriterBase
 
-class CSVWriterUserDemographics(CSVWriterBase):
+class CSVWriterGeoDemographics(CSVWriterBase):
 
     def __init__(self,
                  output_file_name=None,
                  output_directory_name=None,
-                 user_demographics_list=None):
+                 geo_demographics_list=None):
 
         CSVWriterBase.__init__(self, output_file_name, output_directory_name)
-        self.__user_demographics_list = user_demographics_list
+        self.__geo_demographics_list = geo_demographics_list
 
     @property
-    def user_demographics_list(self):
-        return self.__user_demographics_list
+    def geo_demographics_list(self):
+        return self.__geo_demographics_list
 
-    @user_demographics_list.setter
-    def user_demographics_list(self, user_demographics_list):
-        self.__user_demographics_list = user_demographics_list
+    @geo_demographics_list.setter
+    def geo_demographics_list(self, geo_demographics_list):
+        self.__geo_demographics_list = geo_demographics_list
 
-
-    def write_user_demographics(self):
+    def write_geo_demographics(self):
 
         output_file_path = '{0}{1}'.format(self.output_directory_name, self.output_file_name)
 
         write_mode = self._get_write_mode(output_file_path)
 
-        fieldnames = ['doi', 'demographic_source', 'demographic_group_type',
-                      'demographic_group_value', 'demographic_total']
+        fieldnames = ['doi', 'geo_demographic_source', 'country_code', 'geo_demographic_total']
 
         try:
 
@@ -39,19 +37,18 @@ class CSVWriterUserDemographics(CSVWriterBase):
 
                     output_writer.writeheader()
 
-                for user_demographics in self.__user_demographics_list:
+                for geo_demographics in self.__geo_demographics_list:
 
-                    output_dict = dict(doi=user_demographics.doi,
-                                       demographic_source=user_demographics.source,
-                                       demographic_group_type=user_demographics.group_type,
-                                       demographic_group_value=user_demographics.group_value,
-                                       demographic_total=user_demographics.total)
+                    output_dict = dict(doi=geo_demographics.doi,
+                                       geo_demographic_source=geo_demographics.source,
+                                       country_code=geo_demographics.country_code,
+                                       geo_demographic_total=geo_demographics.total)
 
                     output_writer.writerow(output_dict)
 
         except:
 
-            print('Something totally unexpected happened when trying to write to a User Demographics CSV file')
+            print('Something totally unexpected happened when trying to write to a Geo Demographics CSV file')
             print('The writer was setup to write to a file called {0}{1}'.format(self.output_directory_name,
                                                                                  self.output_file_name))
             if write_mode == 'a':
